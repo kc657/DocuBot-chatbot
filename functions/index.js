571253493c0232d34a56048a0601445f900b9f7c
kc657 <kvn.0218@gmail.com>
2017-11-12 02:03:29 -0800

    Attempt to add tellslack function

--- functions/index.js
+++ functions/index.js
@@ -6,15 +6,19 @@ const config = require('./env.json')
 const strings = require('./strings')
 
 const Actions = {
-  TELL_FACEBOOK: 'tell.facebook'
+  TELL_FACEBOOK: 'tell.facebook',
+  TELL_SLACK: 'tell.slack'
 }
 
 const Contexts = {
-  FACEBOOK: 'choose_facebook-followup'
+  FACEBOOK: 'choose_facebook-followup',
+  SLACK: 'choose_slack-followup'
 }
 
 const Parameters = {
-  PLATFORM: 'platform'
+  PLATFORM: 'platform',
+  FACEBOOKPLATFORM: 'facebookPlatform',
+  SLACKPLATFORM: 'slackPlatform'
 }
 
 const Lifespans = {
@@ -24,39 +28,59 @@ const Lifespans = {
 
 process.env.DEBUG = 'actions-on-google:*'
 
+const getRandomValue = array => array[0]
+
+const getRandomFact = instructions => {
+  if (!instructions.length) {
+    return null
+  }
+  const step = getRandomValue(instructions)
+  // Delete the fact from the local data since we now already used it
+  instructions.splice(instructions.indexOf(step), 1)
+  return step
+}
+
 const initData = app => {
   const data = app.data
   if (!data.facts) {
     data.facts = {
-      content: {}
+      content: {},
+      steps: null
     }
   }
-  console.log('data in initData is ', data)
   return data
 }
 
 const tellFacebook = (app) => {
   const data = initData(app)
-  console.log(app)
-  console.log(data)
+  if (!data.facts.steps) {
+    data.facts.steps = strings.facebookDoc.slice()
+  }
+  console.log('LOOK HERE => ', data)
 
-  const parameter = Parameters.PLATFORM
+  const parameter = Parameters.FACEBOOKPLATFORM
   const platformCategory = app.getArgument(parameter)
   console.log('params is ', platformCategory)
+  const fact = getRandomFact()
+  app.ask(strings.facebookDoc[0].steps)
+}
 
-  const msg = 'HELLO WORLD FROM TELLFACEBOOK'
-  app.ask(strings.facebookDoc[0].category)
+const tellSlack = (app) => {
+  const data = initData(app)
+  console.log('HELLO FROM TELLSLACK')
+  const msg = 'Hello form tellslack'
+  app.ask(msg)
 }
 
 const actionMap = new Map()
 actionMap.set(Actions.TELL_FACEBOOK, tellFacebook)
+actionMap.set(Actions.TELL_SLACK, tellSlack)
 
 const documentationDashbot
  = functions.https.onRequest((request, response) => {
    const app = new DialogflowApp({ request, response })
-   console.log('APP IS ', app)
-   console.log(`Request headers: ${JSON.stringify(request.headers)}`)
-   console.log(`Request body: ${JSON.stringify(request.body)}`)
+  //  console.log(`Request headers: ${JSON.stringify(request.headers)}`)
+  //  console.log(`Request body: ${JSON.stringify(request.body)}`)
    app.handleRequest(actionMap)
  })
 
